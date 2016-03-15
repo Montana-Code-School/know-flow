7333e854a8017559a9662bddc8bf9b0c85721650
Jay Cheroske <jay@portal.org>
2016-03-15 09:49:39 -0600

    messing with icons

--- client/jsx/AccessMarker.jsx
+++ client/jsx/AccessMarker.jsx
@@ -1,13 +1,22 @@
 'use strict';
 
 const {Colors} = MUI.Styles;
+const {SvgIcons} = MUI.Libs;
 
 const COLORS = {
-  ready: Colors.blue500,
+  ready: Colors.teal500,
   selected: Colors.orange500,
   putIn: Colors.green500,
   takeOut: Colors.red500,
   cancel: Colors.blueGrey500
+};
+
+const ICONS = {
+  ready: SvgIcons.MapsPlace,
+  selected: SvgIcons.MapsBeenHere,
+  putIn: SvgIcons.MapsBeenHere,
+  takeOut: SvgIcons.MapsBeenHere,
+  cancel: SvgIcons.MapsPlace
 };
 
 Globals.AccessMarker = React.createClass({
@@ -19,7 +28,6 @@ Globals.AccessMarker = React.createClass({
   },
 
   render() {
-    const {SvgIcons} = MUI.Libs;
     const {access, mode, onClick} = this.props;
 
     const wrappedHandler = (event) => {
@@ -27,14 +35,37 @@ Globals.AccessMarker = React.createClass({
       onClick(event)
     };
 
+    const icon = SvgIcons.MapsPlace // ICONS[mode];
+    const iconColor = COLORS[mode];
+
+    console.log(icon);
+
     return (
       <Marker latlng={[access.lat, access.lng]} onClick={wrappedHandler}>
-        <MarkerDivIcon iconSize={[48,48]} iconAnchor={[24,42]}><SvgIcons.MapsPlace color={COLORS[mode]} style={{height: 48, width: 48}} /></MarkerDivIcon>
+        <MarkerDivIcon iconSize={[48,48]} iconAnchor={[24,42]}>{this._iconForMode()}</MarkerDivIcon>
 
         <MarkerLabel direction={access.labelSettings.direction} offset={access.labelSettings.offset} noHide={true}>
           <div>{this.props.access.name}</div>
         </MarkerLabel>
       </Marker>
     )
+  },
+
+  _iconForMode() {
+    const mode = this.props.mode;
+
+    if (mode === 'ready') {
+      return <SvgIcons.MapsPlace color={COLORS[mode]} style={{height: 48, width: 48}} />
+    } else if (mode === 'selected') {
+      return  <SvgIcons.MapsBeenhere color={COLORS[mode]} style={{height: 48, width: 48}} />
+    } else if (mode === 'putIn') {
+      return <SvgIcons.MapsBeenhere color={COLORS[mode]} style={{height: 48, width: 48}} />
+    } else if (mode === 'takeOut') {
+      return <SvgIcons.MapsBeenhere color={COLORS[mode]} style={{height: 48, width: 48}} />
+    } else if (mode === 'cancel') {
+      return <SvgIcons.MapsPlace color={COLORS[mode]} style={{height: 48, width: 48}} />
+    } else {
+      throw new Error('Unknown AccessMarker mode: ' + mode);
+    }
   }
 });
