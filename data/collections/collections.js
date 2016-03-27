da3c3aedbf3c36cbe7c54789cb742a93c032bb4b
Jay Cheroske <jay@portal.org>
2016-03-27 09:51:00 -0600

    added security package

--- data/collections/collections.js
+++ data/collections/collections.js
@@ -31,12 +31,11 @@ if (Meteor.isServer) {
   Meteor.publish(null, function() {
     return Meteor.users.find({ _id: this.userId }, { fields: { 'services.facebook': 1 } })
   });
-/*
+
   Rivers.permit(['insert', 'update', 'remove']).never().apply();
   RiverAccesses.permit(['insert', 'update', 'remove']).never().apply();
   RiverInstruments.permit(['insert', 'update', 'remove']).never().apply();
   RiverData.permit(['insert', 'update', 'remove']).never().apply();
   UserTrips.permit(['insert']).apply();
   UserTrips.permit(['update', 'remove']).never().apply();
-  */
 }
